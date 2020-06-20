23668892505f2fbd3ad5a935118b131447cf4de6
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-06-20 13:37:04 +0300

    added files for vue cli

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Let's see you Vue Yourself"/>
   </div>
 </template>
 
